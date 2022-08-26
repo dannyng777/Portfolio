153c866420c7d1f94fbd9a115f3d9b1c68f7a335
Danh Nguyen <dannyng777@gmail.com>
2022-08-25 19:03:12 -0500

    portfolio in progress

---
+++ algor.js
@@ -0,0 +1,11 @@
+//Without Helper Methods
+let test = [4,5,1,9];
+
+console.log(test[test.length-1])
+const plusOne = (digits)=>{
+    if(test[test.length-1]==9){
+        return test[test.length-1]+1 
+    }
+}
+
+console.log(plusOne(test));
